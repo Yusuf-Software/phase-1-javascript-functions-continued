7f0afdb65683c515672250ade8232be207fc63a2
Yousif A. Dhahir <Yusuf.it.01@gmail.com>
2022-08-10 10:47:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    let want = 'This Saturday, I want to ' + fun + '!';
+    return (want)
+}
+
+function mondayWork(work = 'go to the office') {
+    let want = 'This Monday, I will ' + work + '.';
+    return (want);
+}
+
+function wrapAdjective(highlight = '*') {
+    return function (bla = 'special') {
+        return `You are ${highlight}${bla}${highlight}!`;
+    }
+}
